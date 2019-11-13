dc196c11c07ad9052330bf2570a6ececafceb1e1
Veenman <Serge.Veenman@ing.com>
2019-11-13 09:59:34 +0100

    test_py_script

---
+++ hello_world.py
@@ -0,0 +1,27 @@
+def hello_world():
+    return 'hello world'
+
+print(hello_world())
+import matplotlib.pyplot as plt
+
+from numpy import matrix
+A = matrix( [[1,2,3],[11,12,13],[21,22,23]]) # Creates a matrix.
+x = matrix( [[1],[2],[3]] )                  # Creates a matrix (like a column vector).
+y = matrix( [[1,2,3]] )                      # Creates a matrix (like a row vector).
+print("A=", A)
+print("x=", x)
+print("y=", y)
+
+
+#
+#
+# import sys
+# print (sys.version)
+
+# strings
+#f = "hugo"
+#print(f)
+
+#f[1] # all strings in python are lists of characters
+
+
